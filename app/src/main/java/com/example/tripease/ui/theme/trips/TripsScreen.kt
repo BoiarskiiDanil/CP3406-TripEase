b9ad3252b1433c5e0b4d0a49cd6e110b29c04cf8
danil <danil.boiarskii@my.jcu.edu.au>
2025-12-16 20:46:39 +0800

    Create separate pages for Trips feature and Add Trip function

--- app/src/main/java/com/example/tripease/ui/theme/trips/TripsScreen.kt
+++ app/src/main/java/com/example/tripease/ui/theme/trips/TripsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.tripease.viewmodel.TripEaseViewModel
 
+
 @Composable
 fun TripsScreen(
     viewModel: TripEaseViewModel,
