719aa1447df9ffda589608abc2d69127bd13aab0
danil <danil.boiarskii@my.jcu.edu.au>
2025-12-16 20:47:17 +0800

    Create models for both Expense and Trip feature

--- app/src/main/java/com/example/tripease/model/Trip.kt
+++ app/src/main/java/com/example/tripease/model/Trip.kt
@@ -3,6 +3,7 @@ package com.example.tripease.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+
 @Entity(tableName = "trips")
 data class Trip(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
